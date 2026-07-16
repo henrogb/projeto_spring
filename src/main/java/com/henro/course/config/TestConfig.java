c1890645def8054aaf116bf0cb4fedcbcb65ca07
Henro Gabriel Lague Isquierdo <henro.isquierdo@isdra.com.br>
2026-07-16 12:59:51 -0300

    .

--- src/main/java/com/henro/course/config/TestConfig.java
+++ src/main/java/com/henro/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.henro.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.henro.course.entities.Order;
 import com.henro.course.entities.User;
 import com.henro.course.repositories.OrderRepository;
+import com.henro.course.repositories.UserRepository;
 
 //classe de configuração para o perfil de teste
 //a classe de configuração é uma classe auxiliar
@@ -19,14 +22,20 @@ public class TestConfig implements CommandLineRunner{
 
     //injeção de depencia "automatica" do spring
     @Autowired
-    private OrderRepository userRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Override
     //impletmentação do método do ComandLineRunner
     public void run(String... args) throws Exception {
-       User u1 = new User(null, "Maria Brown", "maria@gmail.com", "519999988", "12345");
-       User u2 = new User(null, "zezinho zeluko", "zeliki@gmail.com", "511231988", "122225");
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "519999988", "12345");
+        User u2 = new User(null, "zezinho zeluko", "zeliki@gmail.com", "511231988", "122225");
+        Order o1 = new Order(null, Instant.parse( "2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse( "2019-06-20T19:53:07Z"), u2);
        //
        userRepository.saveAll(Arrays.asList(u1, u2));
+       orderRepository.saveAll(Arrays.asList(o1, o2));
     }
 }
--- src/main/java/com/henro/course/repositories/OrderRepository.java
+++ src/main/java/com/henro/course/repositories/OrderRepository.java
@@ -1,10 +1,10 @@
 package com.henro.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.henro.course.entities.User;
+import com.henro.course.entities.Order;
 
 //interface que implementa a interação de dados OBJ/BD
-public interface OrderRepository extends JpaRepository<User, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long>{
 
     
 } 
